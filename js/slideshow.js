22806ff76e66e7b11df434c1a3a2a3c5c2421120
gautamdb <gautam@posteo.de>
2021-05-29 12:08:32 +0200

    Completed until Section 1.1

--- js/slideshow.js
+++ js/slideshow.js
@@ -13,8 +13,10 @@ function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
+  //if (n > slides.length) {slideIndex = 1}
+  if (n > slides.length) {slideIndex = slides.length}  
+  //if (n < 1) {slideIndex = slides.length}
+  if (n < 1) {slideIndex = 1}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";  
   }
